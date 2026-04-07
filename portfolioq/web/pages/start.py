bbc1b7a4096d32f4040d595715360f9df8b2d9cc
Maciej Nalepa <12584491+Maciej-GBS@users.noreply.github.com>
2026-04-07 21:12:29 +0200

    start frontend to load ibkr client

--- portfolioq/web/pages/start.py
+++ portfolioq/web/pages/start.py
@@ -3,19 +3,35 @@ import portfolioq.mw as qmw
 from portfolioq.web.context import get_dividends_table, get_trade_table, reset_db
 from portfolioq.web.context import get_currency_converter
 
+def load_nbp_data(files: list):
+    converter = get_currency_converter()
+    for f in files:
+        converter.load_nbp_table(f)
+
 @st.fragment
 def nbp_data_frontend():
     st.markdown("### Currency rates")
     data = st.file_uploader("Official NBP csv tables", accept_multiple_files=True, type="csv")
-    converter = get_currency_converter()
+    load_nbp_data(data)
     for f in data:
-        converter.load_nbp_table(f)
         st.info(f"Loaded successfully: {f.name}")
+
+def load_ibkr_data(statements: list):
+    trade_stream = qmw.IbkrTradeStream(statements)
+    with get_trade_table() as tab:
+        tab.insert(list(trade_stream))
+    with get_dividends_table() as tab:
+        for stmt in statements:
+            dividend_stream = qmw.IbkrDividendStream(stmt)
+            tab.insert(list(dividend_stream))
 
 @st.fragment
 def ibkr_data_frontend():
     st.markdown("### IBKR Client")
-    # st.file_uploader()
+    data = st.file_uploader("Account activity statements in csv", accept_multiple_files=True, type="csv")
+    load_ibkr_data(data)
+    for f in data:
+        st.info(f"Loaded successfully: {f.name}")
 
 def generate_mock_data(num_dividends: int, num_trades: int):
     dividend_stream = qmw.MockDividendStream()
@@ -27,7 +43,7 @@ def generate_mock_data(num_dividends: int, num_trades: int):
 
 @st.fragment
 def mock_data_frontend():
-    st.markdown("### Mock your data")
+    st.markdown("### Mock Client")
     n_dividends = st.number_input("# of dividends", min_value=0)
     n_trades = st.number_input("# of trades", min_value=0)
 
